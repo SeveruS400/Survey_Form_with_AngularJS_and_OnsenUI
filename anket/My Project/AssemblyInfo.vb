cc0283b08e6d5b4f6f58acfaa9823ad0c137539a
SeveruS400 <68468309+SeveruS400@users.noreply.github.com>
2020-09-25 23:27:08 +0300

    Onsen UI

---
+++ anket/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("anket")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("anket")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("83b10961-3e14-4451-853b-08a62ba0d5f4")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ anket/controller/app.js
@@ -0,0 +1,12 @@
+﻿var module = angular.module('my-app', ['onsen']);
+module.controller('PageController', function ($scope) {
+    $scope.alert = function () {
+        ons.notification.alert({ title: "", message: "Teşekkür ederiz" });
+    };
+
+    $scope.deneme = function () {
+        $scope.navigator.pushpage();
+        $scope.alert("messages");
+    }
+
+});
